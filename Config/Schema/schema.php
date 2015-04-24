9bb24f04217786d4ff27ed2bf62e673cefc8b8dd
Mitsuru Mutaguchi <akagane99@gmail.com>
2015-04-24 16:55:08 +0900

    Modified schema phpdoc

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -7,14 +7,13 @@
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
- * @package app.Plugin.Videos.Config.Schema
  */
 
 /**
  * VideosSchema CakeSchema
  *
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
- * @package app.Plugin.Videos.Config.Schema
+ * @package NetCommons\Videos\Config\Schema
  * @SuppressWarnings(PHPMD.LongVariable)
  */
 class VideosSchema extends CakeSchema {
