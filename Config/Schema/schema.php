2f4bec8b4dcca56aa46eb27765f1e8090e035e14
s-nakajima <nakajimashouhei@gmail.com>
2015-08-12 12:28:18 +0900

    typo

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -28,7 +28,7 @@ class VideosSchema extends CakeSchema {
 /**
  * before
  *
- * @param array $event savent
+ * @param array $event event
  * @return bool
  */
 	public function before($event = array()) {
@@ -38,7 +38,7 @@ class VideosSchema extends CakeSchema {
 /**
  * after
  *
- * @param array $event savent
+ * @param array $event event
  * @return bool
  */
 	public function after($event = array()) {
