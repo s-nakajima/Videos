f6e498675121e0b75932157f4e5e516d62e515df
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-03-14 22:13:09 +0900

    多言語のメール送信を意識したつくりに見直し

--- Controller/Component/VideoMailComponent.php
+++ Controller/Component/VideoMailComponent.php
@@ -9,10 +9,8 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
-App::uses('Component', 'Controller');
-App::uses('NetCommonsMail', 'Mails.Utility');
 App::uses('MailSend', 'Mails.Utility');
-App::uses('WorkflowComponent', 'Workflow.Controller.Component');
+App::uses('MailQueuesComponent', 'Mails.Controller/Component');
 
 /**
  * メール Component
@@ -21,38 +19,7 @@ App::uses('WorkflowComponent', 'Workflow.Controller.Component');
  * @package NetCommons\Mails\Controller\Component
  * @property MailQueuesComponent $MailQueues
  */
-class VideoMailComponent extends Component {
-
-/**
- * @var Controller コントローラ
- */
-	protected $_controller = null;
-
-/**
- * Other components
- * コントローラから読み込んだコンポーネントと違い、コンポーネントからコンポーネントを読み込んだ場合は、 コールバックが呼ばれないことに注意して下さい。
- *
- * @var array
- * @link http://book.cakephp.org/2.0/ja/controllers/components.html#id8
- */
-	public $components = array(
-		'Mails.MailQueues',
-	);
-
-/**
- * Called before the Controller::beforeFilter().
- *
- * @param Controller $controller Instantiating controller
- * @return void
- * @link http://book.cakephp.org/2.0/ja/controllers/components.html#Component::initialize
- */
-	public function initialize(Controller $controller) {
-		// どのファンクションでも $controller にアクセスできるようにクラス内変数に保持する
-		$this->_controller = $controller;
-
-		$this->MailQueues->initialize($controller);
-		$this->MailQueues->startup($controller);
-	}
+class VideoMailComponent extends MailQueuesComponent {
 
 /**
  * メール送信
@@ -61,59 +28,10 @@ class VideoMailComponent extends Component {
  * @return bool 成功 or 失敗
  */
 	public function mail($data) {
-		$status = $this->_controller->Workflow->parseStatus();
-		// 一時保存はメール送らない
-		if ($status == WorkflowComponent::STATUS_IN_DRAFT) {
+		if (! $this->isMailSend()) {
 			return true;
 		}
 
-		$mail = new NetCommonsMail();
-		$mail->initPlugin($data);
-
-		// 通知しない
-		if (! $mail->isMailSend) {
-			return true;
-		}
-
-		$mail = $this->__setTags($mail, $data);
-
-		// --- どのパターンでメールを送りたいかによって実装を変更する
-		// キューに保存（ルーム単位でメール配信）
-//		if (!$this->MailQueues->saveQueueRoomId($mail, $contentKey)) {
-//			return false;
-//		}
-
-		// キューに保存（user単位でメール配信）
-		//		$userId = Current::read('User.id');		//仮
-		//		if (!$this->MailQueues->saveQueueUserId($mail, $contentKey, $userId)) {
-		//			return false;
-		//		}
-
-		// キューに保存（メールアドレス単位でメール配信）
-		$toAddress = 'mutaguchi@opensource-workshop.jp';	// 仮
-		if (!$this->MailQueues->saveQueueToAddress($mail, $data['Video']['key'], $toAddress)) {
-			return false;
-		}
-
-		// キューに保存しないで直送信
-		//		$toAddress = 'mutaguchi@opensource-workshop.jp';	// 仮
-		//		$mail->to($toAddress);
-		//		$mail->sendMail();
-
-		// キューからメール送信
-		MailSend::send();
-
-		return true;
-	}
-
-/**
- * タグ セット
- *
- * @param NetCommonsMail $mail NetCommonsメール
- * @param array $data コンテンツデータ
- * @return NetCommonsMail
- */
-	private function __setTags(NetCommonsMail $mail, $data) {
 		// fullpassのURL
 		$url = NetCommonsUrl::actionUrl(array(
 			'controller' => 'videos',
@@ -125,10 +43,45 @@ class VideoMailComponent extends Component {
 		$url = NetCommonsUrl::url($url, true);
 
 		// 定型文の変換タグをセット
-		$mail->assignTag('X-SUBJECT', $data['Video']['title']);
-		$mail->assignTag('X-BODY', $data['Video']['description']);
-		$mail->assignTag('X-URL', $url);
+		//		$this->assignTag('X-SUBJECT', $data['Video']['title']);
+		//		$this->assignTag('X-BODY', $data['Video']['description']);
+		//		$this->assignTag('X-URL', $url);
+		//
+		//		$this->setTags($data);
+		$this->tags = array(
+			'X-SUBJECT' => $data['Video']['title'],
+			'X-BODY' => $data['Video']['description'],
+			'X-URL' => $url,
+		);
+		$this->setWorkflowCommentTag($data);
 
-		return $mail;
+		// --- どのパターンでメールを送りたいかによって実装を変更する
+		// キューに保存（ルーム単位でメール配信）
+//		if (!$this->saveQueueRoomId($contentKey)) {
+//			return false;
+//		}
+
+		// キューに保存（user単位でメール配信）
+		//		$userId = Current::read('User.id');		//仮
+		//		if (!$this->saveQueueUserId($contentKey, $userId)) {
+		//			return false;
+		//		}
+
+		// キューに保存（メールアドレス単位でメール配信）
+		$toAddress = 'mutaguchi@opensource-workshop.jp';	// 仮
+		if (!$this->saveQueueToAddress($data['Video']['key'], $toAddress)) {
+			return false;
+		}
+
+		// キューに保存しないで直送信
+		//		$toAddress = 'mutaguchi@opensource-workshop.jp';	// 仮
+		//		$mail->to($toAddress);
+		//		$mail->sendMail();
+
+
+		// キューからメール送信
+		MailSend::send();
+
+		return true;
 	}
 }
