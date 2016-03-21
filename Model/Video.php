398c65358c2c22a8ac7becf29266de925309f57c
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-03-21 21:55:08 +0900

    validateエラー時のロールバックは不要のため削除

--- Model/Video.php
+++ Model/Video.php
@@ -247,7 +247,6 @@ class Video extends VideosAppModel {
 		//バリデーション
 		$this->set($data);
 		if (! $this->validates(array('add'))) {
-			$this->rollback();
 			return false;
 		}
 
@@ -291,7 +290,6 @@ class Video extends VideosAppModel {
 		$this->set($data);
 		/* @see beforeValidate() */
 		if (! $this->validates(array('edit'))) {
-			$this->rollback();
 			return false;
 		}
 
