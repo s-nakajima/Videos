1fe9f2ee914f19f18455446287130b5d5fce0759
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-04-27 18:34:22 +0900

    タグの言語指定見直し

--- Controller/VideosController.php
+++ Controller/VideosController.php
@@ -136,7 +136,7 @@ class VideosController extends VideosAppController {
 
 		// カテゴリ名をタイトルに
 		$tag = $this->Video->getTagByTagId($tagId);
-		$this->set('listTitle', __d('tags', 'Tag') . ':' . $tag['Tag']['name']);
+		$this->set('listTitle', __d('blogs', 'tag') . ':' . $tag['Tag']['name']);
 
 		$conditions = array(
 			'Tag.id' => $tagId // これを有効にするにはentry_tag_linkもJOINして検索か。
