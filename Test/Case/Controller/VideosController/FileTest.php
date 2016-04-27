4c93b26661a0a05a9f2a3f624feb91e8cee3ee54
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-04-27 23:05:39 +0900

    テスト追加 VideosController/FileTest

---
+++ Test/Case/Controller/VideosController/FileTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * VideosController::file()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * VideosController::file()のテスト
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Videos\Test\Case\Controller\VideosController
+ */
+class VideosControllerFileTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.videos.video',
+		'plugin.videos.video_block_setting',
+		'plugin.videos.video_frame_setting',
+		'plugin.likes.like',
+		'plugin.likes.likes_user',
+		'plugin.tags.tag',
+		'plugin.tags.tags_content',
+		'plugin.content_comments.content_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'videos';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'videos';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * テストDataの取得
+ *
+ * @return array
+ */
+	private function __data() {
+		$frameId = '6';
+		$blockId = '2';
+
+		$data = array(
+			'action' => 'file',
+			'frame_id' => $frameId,
+			'block_id' => $blockId,
+			'key' => 'content_key_1',
+			'thumbnail'
+		);
+
+		return $data;
+	}
+
+/**
+ * file()アクションのGetリクエストテスト
+ * - ダウンロード処理のため、プログラム実行のみテスト
+ *
+ * @return void
+ */
+	public function testFileGet() {
+		$this->generate(
+			'Videos.Videos', [
+				'components' => [
+					'Download'
+				]
+			]
+		);
+
+		$this->controller->Components->Download
+			->expects($this->once())
+			->method('doDownload')
+			->will($this->returnValue('test'));
+
+		$urlOptions = $this->__data();
+
+		//テスト実施
+		$this->_testGetAction($urlOptions, null, 'MissingViewException', 'view');
+	}
+
+/**
+ * file()アクションのGetリクエスト 動画データなし例外テスト
+ *
+ * @return void
+ */
+	public function testFileGetNotFoundException() {
+		$data = $this->__data();
+		$data = Hash::insert($data, 'key', 'content_key_999');
+
+		//テスト実行
+		$this->_testGetAction($data, null, 'NotFoundException', 'view');
+	}
+}
