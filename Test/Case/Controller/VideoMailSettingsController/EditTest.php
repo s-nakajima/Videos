f7ffe63c7fdf46ecd060b0be12ca20e4aca1333e
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-04-28 16:51:04 +0900

    テスト追加 VideoMailSettingsController/EditTest

---
+++ Test/Case/Controller/VideoMailSettingsController/EditTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * VideoMailSettingsController::edit()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * VideoMailSettingsController::edit()のテスト
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Videos\Test\Case\Controller\VideoMailSettingsController
+ */
+class VideoMailSettingsControllerEditTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.mails.mail_setting',
+		'plugin.mails.mail_setting_fixed_phrase',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'videos';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'video_mail_settings';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * edit()アクションのGetリクエストテスト
+ *
+ * @return void
+ */
+	public function testEditGet() {
+		//テストデータ
+		$frameId = '6';
+		$blockId = '2';
+
+		//テスト実行
+		$this->_testGetAction(array('action' => 'edit', 'block_id' => $blockId, 'frame_id' => $frameId),
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$this->assertInput('form', null, 'videos/video_mail_settings/edit/' . $blockId, $this->view);
+	}
+
+}
