980e4994027ed785b8ce012dc7ac7f6a8464bb5d
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-05-11 15:07:05 +0900

    アップロードvalidate追加

--- Test/Case/Model/Video/ValidateTest.php
+++ Test/Case/Model/Video/ValidateTest.php
@@ -12,6 +12,7 @@
 App::uses('NetCommonsValidateTest', 'NetCommons.TestSuite');
 App::uses('VideoFixture', 'Videos.Test/Fixture');
 App::uses('Video', 'Videos.Model');
+App::uses('VideoTestUtil', 'Videos.Test/Case');
 
 /**
  * Video::validate()のテスト
@@ -71,20 +72,24 @@ class VideoValidateTest extends NetCommonsValidateTest {
 		$overwrite['Video'][Video::THUMBNAIL_FIELD]['size'] = 0;
 
 		//debug($data);
-		return array(
-			'登録:language_id' => array('data' => $data, 'field' => 'language_id', 'value' => null,
-				'message' => __d('net_commons', 'Invalid request.'),
-				'overwrite' => $overwrite, 'options' => array('add')),
-			'登録:block_id' => array('data' => $data, 'field' => 'block_id', 'value' => null,
-				'message' => __d('net_commons', 'Invalid request.'),
-				'overwrite' => array(), 'options' => array('add')),
-			'登録:title' => array('data' => $data, 'field' => 'title', 'value' => null,
-				'message' => sprintf(__d('net_commons', 'Please input %s.'), __d('videos', 'title')),
-				'overwrite' => array(), 'options' => array('add')),
-			'編集:language_id' => array('data' => $data, 'field' => 'language_id', 'value' => null,
-				'message' => __d('net_commons', 'Invalid request.'),
-				'overwrite' => $overwrite, 'options' => array('edit')),
-		);
+
+		$result['登録:language_id'] = array('data' => $data, 'field' => 'language_id', 'value' => null,
+			'message' => __d('net_commons', 'Invalid request.'),
+			'overwrite' => $overwrite, 'options' => array('add'));
+
+		$result['登録:block_id'] = array('data' => $data, 'field' => 'block_id', 'value' => null,
+			'message' => __d('net_commons', 'Invalid request.'),
+			'overwrite' => array(), 'options' => array('add'));
+
+		$result['登録:title'] = array('data' => $data, 'field' => 'title', 'value' => null,
+			'message' => sprintf(__d('net_commons', 'Please input %s.'), __d('videos', 'title')),
+			'overwrite' => array(), 'options' => array('add'));
+
+		$result['編集:language_id'] = array('data' => $data, 'field' => 'language_id', 'value' => null,
+			'message' => __d('net_commons', 'Invalid request.'),
+			'overwrite' => $overwrite, 'options' => array('edit'));
+
+		return $result;
 	}
 
 /**
@@ -114,6 +119,129 @@ class VideoValidateTest extends NetCommonsValidateTest {
 		//$result = $this->$model->validates();
 		$result = $this->$model->validates($options);
 		$this->assertFalse($result);
+
+		if ($message) {
+			$this->assertEquals($this->$model->validationErrors[$field][0], $message);
+		}
+	}
+
+/**
+ * ValidationUploadErrorのDataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - field フィールド名
+ *  - value セットする値
+ *  - message エラーメッセージ
+ *  - overwrite 上書きするデータ(省略可)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderValidationUploadError() {
+		$data['Video'] = (new VideoFixture())->records[0];
+
+		$testUtil = new VideoTestUtil();
+		$data['Video'][Video::VIDEO_FILE_FIELD] = $testUtil->getFileData('Videos', 'video1.mp4', 'video/mp4');
+		$data['Video'][Video::THUMBNAIL_FIELD] = $testUtil->getFileData('Videos', 'thumbnail1.jpg', 'image/jpeg');
+
+		//		$data['Video'][Video::VIDEO_FILE_FIELD] = array(
+		//			'name' => 'video1.mp4',
+		//			'type' => 'video/mp4',
+		//			'tmp_name' => '',
+		//			'error' => 0,
+		//			'size' => 4544587,
+		//		);
+		//debug($data);
+
+		// --- 登録, Ffmpeg=ON
+		$overwrite = array();
+		$overwrite['Video'][Video::VIDEO_FILE_FIELD]['error'] = 1;
+		$result['登録, Ffmpeg=ON:video_file - ファイルなし'] = array('data' => $data, 'field' => Video::VIDEO_FILE_FIELD,
+			'message' => __d('files', 'Please specify the file'),
+			'overwrite' => $overwrite, 'options' => array('add'), 'isFfmpegEnable' => true);
+
+		$overwrite = array();
+		$overwrite['Video'][Video::VIDEO_FILE_FIELD]['name'] = 'thumbnail1.jpg';
+		$result['登録, Ffmpeg=ON:video_file - 拡張子違い'] = array('data' => $data, 'field' => Video::VIDEO_FILE_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => $overwrite, 'options' => array('add'), 'isFfmpegEnable' => true);
+
+		$data['Video'][Video::VIDEO_FILE_FIELD] = $testUtil->getFileData('Videos', 'thumbnail1.mp4', 'video/mp4');
+		$result['登録, Ffmpeg=ON:video_file - 拡張子偽装によるmimetype違い'] = array('data' => $data, 'field' => Video::VIDEO_FILE_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => array(), 'options' => array('add'), 'isFfmpegEnable' => true);
+
+		// video_fileを正常なデータにセットし直す
+		$data['Video'][Video::VIDEO_FILE_FIELD] = $testUtil->getFileData('Videos', 'video1.mp4', 'video/mp4');
+
+		// --- 登録, Ffmpeg=OFF
+		$overwrite = array();
+		$overwrite['Video'][Video::THUMBNAIL_FIELD]['error'] = 1;
+		$result['登録, Ffmpeg=OFF:thumbnail - ファイルなし'] = array('data' => $data, 'field' => Video::THUMBNAIL_FIELD,
+			'message' => __d('files', 'Please specify the file'),
+			'overwrite' => $overwrite, 'options' => array('add'), 'isFfmpegEnable' => false);
+
+		$overwrite = array();
+		$overwrite['Video'][Video::THUMBNAIL_FIELD]['name'] = 'video1.mp4';
+		$result['登録, Ffmpeg=OFF:thumbnail - 拡張子違い'] = array('data' => $data, 'field' => Video::THUMBNAIL_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => $overwrite, 'options' => array('add'), 'isFfmpegEnable' => false);
+
+		$data['Video'][Video::THUMBNAIL_FIELD] = $testUtil->getFileData('Videos', 'video1.jpg', 'image/jpeg');
+		$result['登録, Ffmpeg=OFF:thumbnail - 拡張子偽装によるmimetype違い'] = array('data' => $data, 'field' => Video::THUMBNAIL_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => array(), 'options' => array('add'), 'isFfmpegEnable' => false);
+
+		// thumbnailを正常なデータにセットし直す
+		$data['Video'][Video::THUMBNAIL_FIELD] = $testUtil->getFileData('Videos', 'thumbnail1.jpg', 'image/jpeg');
+
+		// --- 編集
+		$overwrite = array();
+		$overwrite['Video'][Video::THUMBNAIL_FIELD]['name'] = 'video1.mp4';
+		$result['編集:thumbnail - 拡張子違い'] = array('data' => $data, 'field' => Video::THUMBNAIL_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => $overwrite, 'options' => array('edit'), 'isFfmpegEnable' => true);
+
+		$data['Video'][Video::THUMBNAIL_FIELD] = $testUtil->getFileData('Videos', 'video1.jpg', 'image/jpeg');
+		$result['編集:thumbnail - 拡張子偽装によるmimetype違い'] = array('data' => $data, 'field' => Video::THUMBNAIL_FIELD,
+			'message' => __d('files', 'It is upload disabled file format'),
+			'overwrite' => array(), 'options' => array('edit'), 'isFfmpegEnable' => true);
+
+		return $result;
+	}
+
+/**
+ * Validates uploadのテスト
+ *
+ * @param array $data 登録データ
+ * @param string $field フィールド名
+ * @param string $message エラーメッセージ
+ * @param array $overwrite 上書きするデータ
+ * @param array $options validateのオプション
+ * @param bool|int $isFfmpegEnable FFMPEG有効フラグ
+ * @dataProvider dataProviderValidationUploadError
+ * @return void
+ */
+	public function testValidationUploadError($data, $field, $message, $overwrite = array(), $options = array(), $isFfmpegEnable = 1) {
+		//public function testValidationUploadError($data, $field, $value, $message, $overwrite = array(), $options = array()) {
+		$model = $this->_modelName;
+
+		//		if (is_null($value)) {
+		//			unset($data[$model][$field]);
+		//		} else {
+		//			$data[$model][$field] = $value;
+		//		}
+		$data = Hash::merge($data, $overwrite);
+
+		$this->$model->isFfmpegEnable = $isFfmpegEnable;
+
+		//validate処理実行
+		$this->$model->set($data);
+		//$result = $this->$model->validates();
+		$result = $this->$model->validates($options);
+		$this->assertFalse($result);
+
+		//var_dump($this->$model->validationErrors);
 
 		if ($message) {
 			$this->assertEquals($this->$model->validationErrors[$field][0], $message);
