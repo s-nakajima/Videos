7c73f481764737c8985c9c8d88ea7ff7d48b9be6
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-06-10 09:26:16 +0900

    ブロック削除時、カテゴリ削除

--- Model/VideoBlockSetting.php
+++ Model/VideoBlockSetting.php
@@ -43,6 +43,8 @@ class VideoBlockSetting extends VideosAppModel {
 			// save, delete時にloadModels()
 			// delete時にblock_id, block_keyで紐づいてるデータ削除
 			'loadModels' => array(
+				'Category' => 'Categories.Category',
+				'CategoryOrder' => 'Categories.CategoryOrder',
 				'ContentComment' => 'ContentComments.ContentComment',
 				'Like' => 'Likes.Like',
 				'Tag' => 'Tags.Tag',
